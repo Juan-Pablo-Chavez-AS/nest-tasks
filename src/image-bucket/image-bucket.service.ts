6dd4fd9cd862e7f558e96694e4b894f2943afb09
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2024-01-24 10:52:05 -0400

    Cloudfare keys moved to .env file

--- src/image-bucket/image-bucket.service.ts
+++ src/image-bucket/image-bucket.service.ts
@@ -5,23 +5,17 @@ import { MemoryStoredFile } from 'nestjs-form-data';
 
 @Injectable()
 export class ImageBucketService {
-  static readonly accountId = 'dfeb185a6751a7eeb59e6a61be6bc96c';
-  static readonly accessKeyId = '4019d989be220432bd0168ca43e14a5e';
-  static readonly secretAccessKey =
-    '61c831572db724707e286ce531a95aa45b2551e807d56aad4f49c7974701f138';
-  static readonly bucketName = 'task-poc';
-
   async uploadImage(image: MemoryStoredFile, name: string) {
     const client = new S3Client({
       region: 'auto',
-      endpoint: `https://${ImageBucketService.accountId}.r2.cloudflarestorage.com`,
+      endpoint: `https://${process.env.CLOUDFARE_ACCOUNT_ID}.r2.cloudflarestorage.com`,
       credentials: {
-        accessKeyId: ImageBucketService.accessKeyId,
-        secretAccessKey: ImageBucketService.secretAccessKey,
+        accessKeyId: process.env.CLOUDFARE_ACCESS_KEY_ID,
+        secretAccessKey: process.env.CLOUDFARE_SECRET_ACCESS_KEY,
       },
     });
     const command = new PutObjectCommand({
-      Bucket: ImageBucketService.bucketName,
+      Bucket: process.env.CLOUDFARE_IMAGE_BUCKET_NAME,
       Key: `${name}.png`,
     });
 
