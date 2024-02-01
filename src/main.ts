a93a78208dbfe443712703730b10dc4cfab8db3b
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2024-02-01 17:46:42 -0400

    Cors enabled in the API

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { QueryFailFilter } from './query-fail/query-fail.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new QueryFailFilter());
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
